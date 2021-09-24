08414ef4eea9111fcb88640ccafd887aa827150a
João Victor <85710850+VictorBLima@users.noreply.github.com>
2021-09-24 19:33:13 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <p className="lead">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
               rutrum mi felis, non lobortis enim congue in. Vestibulum
-              scelerisque faucibus quam, eu lobortis ante facilisis facilesis.
+              scelerisque faucibuss quam, eu lobortis ante facilisis facilesis.
               Phasellus porttitor tortor ets.
             </p>
           </div>
