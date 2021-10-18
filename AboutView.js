a666be6329fc2b9b6d4c5ee92f30082c78ba5155
João Victor <85710850+VictorBLima@users.noreply.github.com>
2021-10-18 17:52:06 -0300

    Update AboutView.js

--- AboutView.js
+++ AboutView.js
@@ -9,7 +9,7 @@ const AboutView = () => {
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
               Lorem ipsum dolores sit amet, consecteturs adipiscing eltes. Aliquen
-              feugiat, enim volutpat volutpat ullamcorper, arcu arcu bibendum
+              feugiat, enim volutpat volutpat ullamcorper, arcus arcu bibendum
               augue, sed tempora tortor massa nec dolor. Duis sapien nisl, luctus
               in semper vitae, pharetra eget lacus. Nullams odio eros, molis at
               felis nonn, ultrices tristiquis libero. Nulas qui pulvinar rizas.
