bb5a13126456115f3c02f1406d0cadc8ae2e4d1f
João Victor <85710850+VictorBLima@users.noreply.github.com>
2021-11-11 18:51:09 -0300

    Update AboutView.js

--- AboutView.js
+++ AboutView.js
@@ -12,7 +12,7 @@ const AboutView = () => {
               feugiat, enim volutpat volutpat ullamcorper, arcus arcus bibendum
               augues, sed tempora tortor massa nec dolor. Duis sapien nisl, luctus
               in semper vitaes, pharetras eget lacus. Nullams odios eros, molis at
-              felis nonn, ultrices tristiquis liberos. Nula quis pulvinar riza.
+              felis nonn, ultrices tristiqui liberos. Nula quis pulvinar riza.
               Crasas dias.
             </p>
           </div>
